7cce403b7f2f62008e794991bb46c1f396678063
hong4cong <hong4cong@gmail.com>
2015-08-24 14:50:31 +0800

    update

--- MJRefresh/MJRefreshConst.m
+++ MJRefresh/MJRefreshConst.m
@@ -25,4 +25,4 @@ NSString *const MJRefreshAutoFooterNoMoreDataText = @"已经全部加载完毕";
 NSString *const MJRefreshBackFooterIdleText = @"上拉可以加载更多";
 NSString *const MJRefreshBackFooterPullingText = @"松开立即加载更多";
 NSString *const MJRefreshBackFooterRefreshingText = @"正在加载更多的数据...";
-NSString *const MJRefreshBackFooterNoMoreDataText = @"已经全部加载完毕";
+NSString *const MJRefreshBackFooterNoMoreDataText = @"已经全部加载完毕"; 
